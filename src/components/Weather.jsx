2056e0c8338b674086902bc73436187194670f7e
Dhruv Akbari <dhruvakbari303@gmail.com>
2023-11-07 23:33:57 +0530

    convert in tailwind css

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,28 +5,48 @@ const Weather = ({ weatherData }) => {
     const windDirStyle = {
         transform: `rotate(${weatherData.wind.deg + 90}deg)`,
     };
+    const date = new Date(weatherData.dt * 1000);
+
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true, // To display time in 12-hour format with AM/PM
+    };
+    const formattedDate = date.toLocaleDateString('en-US', options);
     return (
         <div className="weather-data">
-            <h2 className="city">{weatherData.name}, {weatherData.sys.country}</h2>
-            <h4 className="weather">{weatherData.weather[0].description}</h4>
-            <img src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="Weather Icon" />
-            <h1 className="temp">{weatherData.main.temp} °C</h1>
-            <div className="temp-container">
-                <div className="feature">
-                    <h4 className="title">Lowest Temperature</h4>
-                    <p className="desc">{weatherData.main.temp_min} °C</p>
+
+            <h2 className="city mb-0 mx-0 my-2 text-3xl text-white uppercase font-semibold tracking-widest">{weatherData.name}, {weatherData.sys.country}</h2>
+
+            <p className="text-white date text-sm tracking-widest my-3">{formattedDate}</p>
+
+            <p className="temp mt-0 mx-0 text-white text-6xl">{weatherData.main.temp} °C</p>
+
+            <div className="flex justify-center">
+                <img src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="Weather Icon" className=" mb-0 mx-0 w-24 mt-1 drop-shadow-md" />
+            </div>
+            <p className="weather text-white uppercase tracking-widest text-base mb-5">{weatherData.weather[0].description}</p>
+
+            <div className="temp-container flex flex-wrap justify-center gap-4">
+                <div className="border w-[48%] p-4 rounded-lg border-b border-r border-white/10 bg-gradient-to-r from-slate-900/30 to-slate-700/10">
+                    <h4 className="text-white tracking-wider">Lowest Temperature</h4>
+                    <p className="text-white uppercase tracking-widest text-base text-bolder">{weatherData.main.temp_min} °C</p>
                 </div>
-                <div className="feature">
-                    <h4 className="title">Highest Temperature</h4>
-                    <p className="desc">{weatherData.main.temp_max} °C</p>
+                <div className="border w-[48%] p-4 rounded-lg border-b border-r border-white/10 bg-gradient-to-r from-slate-900/30 to-slate-700/10">
+                    <h4 className="text-white tracking-wider">Highest Temperature</h4>
+                    <p className="text-white uppercase tracking-widest text-base text-bolder">{weatherData.main.temp_max} °C</p>
                 </div>
-                <div className="feature">
-                    <h4 className="title">Humidity</h4>
-                    <p className="desc">{weatherData.main.humidity} %</p>
+                <div className="border w-[48%] p-4 rounded-lg border-b border-r border-white/10 bg-gradient-to-r from-slate-900/30 to-slate-700/10">
+                    <h4 className="text-white tracking-wider">Humidity</h4>
+                    <p className="text-white uppercase tracking-widest text-base text-bolder">{weatherData.main.humidity} %</p>
                 </div>
-                <div className="feature">
-                    <h4 className="title">Wind Speed</h4>
-                    <p className="desc">{weatherData.wind.speed} km/h, {weatherData.wind.deg}°</p>
+                <div className="border w-[48%] p-4 rounded-lg border-b border-r border-white/10 bg-gradient-to-r from-slate-900/30 to-slate-700/10">
+                    <h4 className="text-white tracking-wider">Wind Speed</h4>
+                    <p className="text-white uppercase tracking-widest text-base text-bolder">{weatherData.wind.speed} km/h, {weatherData.wind.deg}°</p>
                 </div>
             </div>
         </div >
--- src/components/WeatherForm.jsx
+++ src/components/WeatherForm.jsx
@@ -1,38 +1,37 @@
-    import React, { useState } from "react";
-    import PropTypes from "prop-types";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 
-    const WeatherForm = ({ onSubmit }) => {
+const WeatherForm = ({ onSubmit }) => {
 
-        const [city, setCity] = useState('');
+    const [city, setCity] = useState('');
 
-        const handleCityChange = (e) => {
-            setCity(e.target.value);
-        };
-
-        const handleSubmit = (e) => {
-            e.preventDefault();
-            onSubmit(city);
-        };
-
-        return (
-            <form onSubmit={handleSubmit} className="search-container">
-                <input
-                    type="text"
-                    placeholder="City, state code (if USA), country code"
-                    id="city"
-                    value={city}
-                    onChange={handleCityChange}
-                />
-                <input type="submit" value="search" id="search-btn" />
-
-                {/* <button type="submit" id='search-btn'>Search</button> */}
-            </form>
-        )
-    }
-
-    WeatherForm.propTypes = {
-        onSubmit: PropTypes.func,
+    const handleCityChange = (e) => {
+        setCity(e.target.value);
     };
 
-    export default WeatherForm
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(city);
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="search-container mt-0 mx-0 mb-6 text-base grid grid grid-cols-3 gap-4">
+            <input
+                type="text"
+                placeholder="Enter City"
+                id="city"
+                value={city}
+                onChange={handleCityChange}
+                className="bg-transparent text-white p-3 border-b-2 border-solid font-light col-span-2 outline-none text-base border-white/10 border-0 focus:border-white placeholder:text-white"
+            />
+            <input type="submit" value="search" className="border text-white cursor-pointer rounded-lg border-solid p-3 outline-none text-base border-white/10 bg-gray-300/20" />
+        </form>
+    )
+}
+
+WeatherForm.propTypes = {
+    onSubmit: PropTypes.func,
+};
+
+export default WeatherForm
